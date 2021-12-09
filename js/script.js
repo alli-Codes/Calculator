c3b5fea5a51d55f76c340c9b0ebf858f1482729b
alli-Codes <allisonevans081@gmail.com>
2021-12-09 01:31:02 +0100

    Made some changes here

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
-const Calculator = () => {
-	
+let collection = []
+const Calculator = (data) => {
+	this.data = data
+
+	this.sum = () => {
+		let sumData = this.data.reduce((value, item) => value += item, 0)
+		// this.
+	}
 }

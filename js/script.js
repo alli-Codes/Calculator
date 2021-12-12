4f9c9c4b43a1d77a093dc6681a8b8ce19bfa298b
alli-Codes <allisonevans081@gmail.com>
2021-12-12 21:41:54 +0100

    Added some functions here

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
-let collection = []
-const Calculator = (data) => {
+let collection = [2, 10]
+let answer
+
+const Calculator = function(data) {
 	this.data = data
+	this.answer = 0
 
 	this.sum = () => {
 		let sumData = this.data.reduce((value, item) => value += item, 0)
-		// this.
+		console.log(sumData)
+	}
+
+	this.subtract = () => {
+		let subtractData = this.data.reduce((value, item) => value -= item)
+		console.log(subtractData)
 	}
 }
+
+const cal = new Calculator(collection)
+// cal.sum()
+cal.subtract()
